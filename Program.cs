49340e6f0846d1c3c155d763c5c5e2eec29ef0ec
immmdreza <47322990+immmdreza@users.noreply.github.com>
2020-06-28 21:49:29 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //this should write Hello World! on console
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
