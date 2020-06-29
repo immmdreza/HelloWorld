eb4010970aa64d84e73aa1a03fd65ba039e652e4
immmdreza <47322990+immmdreza@users.noreply.github.com>
2020-06-29 12:00:23 +0330

    Added telegram bot

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Telegram.Bot;
 
 namespace HelloWorld
 {
     class Program
     {
+        public static ITelegramBotClient botClient;
+
         static void Main(string[] args)
         {
 
+            botClient = new TelegramBotClient("");
+            
             //this should write Hello World! on console
             Console.WriteLine("Hello World!");
         }
